e4c9ef10f094b7a192843036b17cd3e09b0a7204
Bibek <137331749+immbibek@users.noreply.github.com>
2024-11-08 22:58:34 +0530

    utils adding

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from "dotenv"
-
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
 dotenv.config({
@@ -8,34 +8,15 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000)
+    console.log(`server is running at port : ${process.env.PORT}`)
+})
+.catch((err)=>{
+    console.log("Mongo db connection failed !!!",err);
+})
 
 
 
 
 
-
-/* 
-import express from "express"
-
-const app=express()
-
-this way of wrting is called efis
-(async ()=>{
-   try {
-    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-
-    app.on("ERORR:",()=>{
-        console.log("ERROR",error);
-        throw error
-    })
-
-    app.listen(process.env.PORT,()=>{
-        console.log(`APP is listening on port ${process.env.PORT} `)
-    })
-
-   } catch (error) {
-    console.log("ERROR",error)
-    throw err
-    
-   }
-}) ()  */
